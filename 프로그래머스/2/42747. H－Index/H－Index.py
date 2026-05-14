650922efed0078f7005d5743fb7ab8b850aa513a
hyebinJeong <138184978+hyebinJeong@users.noreply.github.com>
2026-05-14 23:04:04 +0900

    [level 2] Title: H-Index, Time: 2.43 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,13 @@
+def solution(citations):
+    n = len(citations)
+    
+    for h in range(n, -1, -1):
+        
+        count = 0
+        
+        for citation in citations:
+            if citation >= h:
+                count += 1
+        
+        if count >= h:
+            return h
